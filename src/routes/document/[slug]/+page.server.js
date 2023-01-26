a6993cd5f6a132f0166307676454af42155b99c4
DphnZwp <zwuupdaphne@gmail.com>
2023-01-27 00:56:14 +0100

    trying to solve vercel error

--- src/routes/document/[slug]/+page.server.js
+++ src/routes/document/[slug]/+page.server.js
@@ -1,5 +1,6 @@
 import { GraphQLClient, gql } from 'graphql-request';
-import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private';
+import { HYGRAPH_KEY } from '$env/static/private';
+import { HYGRAPH_URL } from '$env/static/private';
 
 export async function load({ params }) {
 	const hygraph = new GraphQLClient(HYGRAPH_URL, {
